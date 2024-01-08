fd9a0bed773835da9e534eb232b298bb1bfcaa2c
RickN50 <rmmen232@learner.dvusd.org>
2024-01-08 09:29:58 -0700

    Enter number make it double

--- Converter/Converter/Program.cs
+++ Converter/Converter/Program.cs
@@ -10,11 +10,16 @@ namespace Converter
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 100; i++)
+            Console.WriteLine("enter your number");
+            int num = 0;
+            int.TryParse(Console.ReadLine(),out num);
+            Console.WriteLine(num * 2);
+            Console.ReadLine();
+            /*for (int i = 0; i < 100; i++)
             {
                 Console.WriteLine(i);
             }
-            Console.ReadLine();
+            Console.ReadLine();*/
         }
     }
 }
