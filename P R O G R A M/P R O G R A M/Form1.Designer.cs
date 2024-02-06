00a28e4c3ec78b023e70e0d516f6c14f1c098b8a
RickN50 <rmmen232@learner.dvusd.org>
2024-02-06 09:07:15 -0700

    P R O G R A M

---
+++ P R O G R A M/P R O G R A M/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace P_R_O_G_R_A_M
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.NametextBox = new System.Windows.Forms.TextBox();
+            this.CharactertextBox = new System.Windows.Forms.TextBox();
+            this.RevenuetextBox = new System.Windows.Forms.TextBox();
+            this.RatingtextBox = new System.Windows.Forms.TextBox();
+            this.CreatortextBox = new System.Windows.Forms.TextBox();
+            this.Namelabel = new System.Windows.Forms.Label();
+            this.Creatorlabel = new System.Windows.Forms.Label();
+            this.Ratinglabel = new System.Windows.Forms.Label();
+            this.Revenuelabel = new System.Windows.Forms.Label();
+            this.Characterlabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(171, 267);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // NametextBox
+            // 
+            this.NametextBox.Location = new System.Drawing.Point(315, 24);
+            this.NametextBox.Name = "NametextBox";
+            this.NametextBox.Size = new System.Drawing.Size(100, 22);
+            this.NametextBox.TabIndex = 1;
+            // 
+            // CharactertextBox
+            // 
+            this.CharactertextBox.Location = new System.Drawing.Point(315, 194);
+            this.CharactertextBox.Name = "CharactertextBox";
+            this.CharactertextBox.Size = new System.Drawing.Size(100, 22);
+            this.CharactertextBox.TabIndex = 2;
+            // 
+            // RevenuetextBox
+            // 
+            this.RevenuetextBox.Location = new System.Drawing.Point(315, 150);
+            this.RevenuetextBox.Name = "RevenuetextBox";
+            this.RevenuetextBox.Size = new System.Drawing.Size(100, 22);
+            this.RevenuetextBox.TabIndex = 3;
+            // 
+            // RatingtextBox
+            // 
+            this.RatingtextBox.Location = new System.Drawing.Point(315, 107);
+            this.RatingtextBox.Name = "RatingtextBox";
+            this.RatingtextBox.Size = new System.Drawing.Size(100, 22);
+            this.RatingtextBox.TabIndex = 4;
+            // 
+            // CreatortextBox
+            // 
+            this.CreatortextBox.Location = new System.Drawing.Point(315, 65);
+            this.CreatortextBox.Name = "CreatortextBox";
+            this.CreatortextBox.Size = new System.Drawing.Size(100, 22);
+            this.CreatortextBox.TabIndex = 5;
+            // 
+            // Namelabel
+            // 
+            this.Namelabel.AutoSize = true;
+            this.Namelabel.Location = new System.Drawing.Point(250, 27);
+            this.Namelabel.Name = "Namelabel";
+            this.Namelabel.Size = new System.Drawing.Size(44, 16);
+            this.Namelabel.TabIndex = 7;
+            this.Namelabel.Text = "Name";
+            // 
+            // Creatorlabel
+            // 
+            this.Creatorlabel.AutoSize = true;
+            this.Creatorlabel.Location = new System.Drawing.Point(250, 68);
+            this.Creatorlabel.Name = "Creatorlabel";
+            this.Creatorlabel.Size = new System.Drawing.Size(51, 16);
+            this.Creatorlabel.TabIndex = 8;
+            this.Creatorlabel.Text = "Creator";
+            // 
+            // Ratinglabel
+            // 
+            this.Ratinglabel.AutoSize = true;
+            this.Ratinglabel.Location = new System.Drawing.Point(250, 110);
+            this.Ratinglabel.Name = "Ratinglabel";
+            this.Ratinglabel.Size = new System.Drawing.Size(46, 16);
+            this.Ratinglabel.TabIndex = 9;
+            this.Ratinglabel.Text = "Rating";
+            // 
+            // Revenuelabel
+            // 
+            this.Revenuelabel.AutoSize = true;
+            this.Revenuelabel.Location = new System.Drawing.Point(247, 153);
+            this.Revenuelabel.Name = "Revenuelabel";
+            this.Revenuelabel.Size = new System.Drawing.Size(62, 16);
+            this.Revenuelabel.TabIndex = 10;
+            this.Revenuelabel.Text = "Revenue";
+            // 
+            // Characterlabel
+            // 
+            this.Characterlabel.AutoSize = true;
+            this.Characterlabel.Location = new System.Drawing.Point(209, 200);
+            this.Characterlabel.Name = "Characterlabel";
+            this.Characterlabel.Size = new System.Drawing.Size(100, 16);
+            this.Characterlabel.TabIndex = 11;
+            this.Characterlabel.Text = "Character/Actor";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(506, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 40);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(506, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 40);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(506, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 40);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(506, 176);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(86, 40);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Characterlabel);
+            this.Controls.Add(this.Revenuelabel);
+            this.Controls.Add(this.Ratinglabel);
+            this.Controls.Add(this.Creatorlabel);
+            this.Controls.Add(this.Namelabel);
+            this.Controls.Add(this.CreatortextBox);
+            this.Controls.Add(this.RatingtextBox);
+            this.Controls.Add(this.RevenuetextBox);
+            this.Controls.Add(this.CharactertextBox);
+            this.Controls.Add(this.NametextBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox NametextBox;
+        private System.Windows.Forms.TextBox CharactertextBox;
+        private System.Windows.Forms.TextBox RevenuetextBox;
+        private System.Windows.Forms.TextBox RatingtextBox;
+        private System.Windows.Forms.TextBox CreatortextBox;
+        private System.Windows.Forms.Label Namelabel;
+        private System.Windows.Forms.Label Creatorlabel;
+        private System.Windows.Forms.Label Ratinglabel;
+        private System.Windows.Forms.Label Revenuelabel;
+        private System.Windows.Forms.Label Characterlabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ P R O G R A M/P R O G R A M/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P_R_O_G_R_A_M
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        struct Movie
+        {
+            public string Name;
+            public string Creator;
+            public string Rating;
+            public string Revenue;
+            public string CharacterActor;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void ShowMovie(Movie m)
+        {
+            NametextBox.Text = m.Name;
+            CreatortextBox.Text = m.Creator;
+            RatingtextBox.Text = m.Rating;
+            RevenuetextBox.Text = m.Revenue;
+            CharactertextBox.Text = m.CharacterActor;
+        }
+    }
+}
