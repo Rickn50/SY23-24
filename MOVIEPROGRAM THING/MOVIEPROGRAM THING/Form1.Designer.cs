57551173eca907ecea770229965f371fc9f95219
RickN50 <rmmen232@learner.dvusd.org>
2024-02-06 09:06:56 -0700

    MOVIESSSSS

---
+++ MOVIEPROGRAM THING/MOVIEPROGRAM THING/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace MOVIEPROGRAM_THING
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Firstbutton = new System.Windows.Forms.Button();
+            this.Nextbutton = new System.Windows.Forms.Button();
+            this.Previousbutton = new System.Windows.Forms.Button();
+            this.Lastbutton = new System.Windows.Forms.Button();
+            this.Savebutton = new System.Windows.Forms.Button();
+            this.NametextBox = new System.Windows.Forms.TextBox();
+            this.CharactertextBox = new System.Windows.Forms.TextBox();
+            this.RevenuetextBox = new System.Windows.Forms.TextBox();
+            this.CreatortextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Currentlabel = new System.Windows.Forms.Label();
+            this.Newbutton = new System.Windows.Forms.Button();
+            this.RatingUpDown = new System.Windows.Forms.NumericUpDown();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.DebugtextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RatingUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(205, 261);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Firstbutton
+            // 
+            this.Firstbutton.Location = new System.Drawing.Point(695, 11);
+            this.Firstbutton.Name = "Firstbutton";
+            this.Firstbutton.Size = new System.Drawing.Size(93, 51);
+            this.Firstbutton.TabIndex = 1;
+            this.Firstbutton.Text = "FIRST";
+            this.Firstbutton.UseVisualStyleBackColor = true;
+            this.Firstbutton.Click += new System.EventHandler(this.Firstbutton_Click_1);
+            // 
+            // Nextbutton
+            // 
+            this.Nextbutton.Location = new System.Drawing.Point(695, 68);
+            this.Nextbutton.Name = "Nextbutton";
+            this.Nextbutton.Size = new System.Drawing.Size(93, 51);
+            this.Nextbutton.TabIndex = 2;
+            this.Nextbutton.Text = "NEXT";
+            this.Nextbutton.UseVisualStyleBackColor = true;
+            this.Nextbutton.Click += new System.EventHandler(this.Nextbutton_Click_1);
+            // 
+            // Previousbutton
+            // 
+            this.Previousbutton.Location = new System.Drawing.Point(695, 125);
+            this.Previousbutton.Name = "Previousbutton";
+            this.Previousbutton.Size = new System.Drawing.Size(93, 51);
+            this.Previousbutton.TabIndex = 3;
+            this.Previousbutton.Text = "PREVIOUS";
+            this.Previousbutton.UseVisualStyleBackColor = true;
+            this.Previousbutton.Click += new System.EventHandler(this.Previousbutton_Click_1);
+            // 
+            // Lastbutton
+            // 
+            this.Lastbutton.Location = new System.Drawing.Point(695, 182);
+            this.Lastbutton.Name = "Lastbutton";
+            this.Lastbutton.Size = new System.Drawing.Size(93, 51);
+            this.Lastbutton.TabIndex = 4;
+            this.Lastbutton.Text = "LAST";
+            this.Lastbutton.UseVisualStyleBackColor = true;
+            this.Lastbutton.Click += new System.EventHandler(this.Lastbutton_Click_1);
+            // 
+            // Savebutton
+            // 
+            this.Savebutton.Location = new System.Drawing.Point(277, 252);
+            this.Savebutton.Name = "Savebutton";
+            this.Savebutton.Size = new System.Drawing.Size(83, 51);
+            this.Savebutton.TabIndex = 5;
+            this.Savebutton.Text = "SAVE";
+            this.Savebutton.UseVisualStyleBackColor = true;
+            this.Savebutton.Click += new System.EventHandler(this.Savebutton_Click_1);
+            // 
+            // NametextBox
+            // 
+            this.NametextBox.Location = new System.Drawing.Point(366, 40);
+            this.NametextBox.Name = "NametextBox";
+            this.NametextBox.Size = new System.Drawing.Size(100, 22);
+            this.NametextBox.TabIndex = 6;
+            // 
+            // CharactertextBox
+            // 
+            this.CharactertextBox.Location = new System.Drawing.Point(366, 211);
+            this.CharactertextBox.Name = "CharactertextBox";
+            this.CharactertextBox.Size = new System.Drawing.Size(100, 22);
+            this.CharactertextBox.TabIndex = 7;
+            // 
+            // RevenuetextBox
+            // 
+            this.RevenuetextBox.Location = new System.Drawing.Point(366, 167);
+            this.RevenuetextBox.Name = "RevenuetextBox";
+            this.RevenuetextBox.Size = new System.Drawing.Size(100, 22);
+            this.RevenuetextBox.TabIndex = 8;
+            // 
+            // CreatortextBox
+            // 
+            this.CreatortextBox.Location = new System.Drawing.Point(366, 82);
+            this.CreatortextBox.Name = "CreatortextBox";
+            this.CreatortextBox.Size = new System.Drawing.Size(100, 22);
+            this.CreatortextBox.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(272, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(272, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 16);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Creator";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(272, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 16);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Rating";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(272, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Revenue";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(251, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 16);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Character/Actor";
+            // 
+            // Currentlabel
+            // 
+            this.Currentlabel.AutoSize = true;
+            this.Currentlabel.Location = new System.Drawing.Point(155, 287);
+            this.Currentlabel.Name = "Currentlabel";
+            this.Currentlabel.Size = new System.Drawing.Size(0, 16);
+            this.Currentlabel.TabIndex = 16;
+            // 
+            // Newbutton
+            // 
+            this.Newbutton.Location = new System.Drawing.Point(367, 252);
+            this.Newbutton.Name = "Newbutton";
+            this.Newbutton.Size = new System.Drawing.Size(99, 51);
+            this.Newbutton.TabIndex = 17;
+            this.Newbutton.Text = "NEW";
+            this.Newbutton.UseVisualStyleBackColor = true;
+            this.Newbutton.Click += new System.EventHandler(this.Newbutton_Click_1);
+            // 
+            // RatingUpDown
+            // 
+            this.RatingUpDown.Location = new System.Drawing.Point(366, 125);
+            this.RatingUpDown.Name = "RatingUpDown";
+            this.RatingUpDown.Size = new System.Drawing.Size(120, 22);
+            this.RatingUpDown.TabIndex = 18;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // DebugtextBox
+            // 
+            this.DebugtextBox.Location = new System.Drawing.Point(544, 298);
+            this.DebugtextBox.Multiline = true;
+            this.DebugtextBox.Name = "DebugtextBox";
+            this.DebugtextBox.Size = new System.Drawing.Size(244, 140);
+            this.DebugtextBox.TabIndex = 19;
+            this.DebugtextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DebugtextBox);
+            this.Controls.Add(this.RatingUpDown);
+            this.Controls.Add(this.Newbutton);
+            this.Controls.Add(this.Currentlabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CreatortextBox);
+            this.Controls.Add(this.RevenuetextBox);
+            this.Controls.Add(this.CharactertextBox);
+            this.Controls.Add(this.NametextBox);
+            this.Controls.Add(this.Savebutton);
+            this.Controls.Add(this.Lastbutton);
+            this.Controls.Add(this.Previousbutton);
+            this.Controls.Add(this.Nextbutton);
+            this.Controls.Add(this.Firstbutton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RatingUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Firstbutton;
+        private System.Windows.Forms.Button Nextbutton;
+        private System.Windows.Forms.Button Previousbutton;
+        private System.Windows.Forms.Button Lastbutton;
+        private System.Windows.Forms.Button Savebutton;
+        private System.Windows.Forms.TextBox NametextBox;
+        private System.Windows.Forms.TextBox CharactertextBox;
+        private System.Windows.Forms.TextBox RevenuetextBox;
+        private System.Windows.Forms.TextBox CreatortextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Currentlabel;
+        private System.Windows.Forms.Button Newbutton;
+        private System.Windows.Forms.NumericUpDown RatingUpDown;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox DebugtextBox;
+    }
+}
+
---
+++ MOVIEPROGRAM THING/MOVIEPROGRAM THING/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MOVIEPROGRAM_THING
+{
+    public partial class Form1 : Form
+    {
+        private int current;
+        private int Count;
+        private Movie[] movies;
+        struct Movie
+        {
+            public string Name;
+            public string Creator;
+            public int Rating;
+            public string Revenue;
+            public string Character;
+            public string PictureBox;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            current = 0;
+            Count = 0;
+            Currentlabel.Text = current.ToString();
+            movies = new Movie[50];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("Movies.txt"))
+            {
+                StreamReader inFIle = new StreamReader("Movies.txt");
+                while (!inFIle.EndOfStream)
+                {
+                    string S = inFIle.ReadLine();
+                    Movie p = Readmovie(S);
+                    movies[Count] = p;
+                    Count++;
+                }
+                inFIle.Close();
+                ShowMovie(movies[0]);
+            }
+        }
+        private Movie Readmovie(string s)
+        {
+            Movie p = new Movie();
+            string[] fields = s.Split('|');
+            p.Name = fields[0];
+            p.Creator = fields[1];
+            p.Rating = int.Parse(fields[2]);
+            p.Revenue = fields[3];
+            p.Character = fields[4];
+            p.PictureBox = fields[5];
+
+            return p;
+
+        }
+        private void Save()
+        {
+            string tmp = "";
+            tmp += NametextBox.Text;
+            tmp += "|";
+            tmp += CreatortextBox.Text;
+            tmp += "|";
+            tmp += RatingUpDown.Value;
+            tmp += "|";
+            tmp += RevenuetextBox.Text;
+            tmp += "|";
+            tmp += CharactertextBox.Text;
+            tmp += "|";
+            tmp += pictureBox1.Image;
+            movies[current] = Readmovie(tmp);
+
+            StreamWriter outfile = new StreamWriter("Movies.txt");
+            for (int i = 0; i < Count; i++)
+            {
+                outfile.WriteLine(MovieToString(movies[i]));
+            }
+            outfile.Close();
+        }
+
+        private string MovieToString(Movie p)
+        {
+            string retVal = "";
+            DebugtextBox.Clear();
+            retVal += p.Name;
+            retVal += "|";
+            retVal += p.Creator;
+            retVal += "|";
+            retVal += p.Rating;
+            retVal += "|";
+            retVal += p.Revenue;
+            retVal += "|";
+            retVal += p.Character;
+            retVal += "|";
+            retVal += pictureBox1;
+            return retVal;
+        }
+        private void ShowMovie(Movie p)
+        {
+            NametextBox.Text = p.Name;
+            CreatortextBox.Text = p.Creator;
+            RatingUpDown.Value = p.Rating;
+            RevenuetextBox.Text = p.Revenue;
+            CharactertextBox.Text = p.Character;
+            if(System.IO.File.Exists(p.PictureBox))
+            {
+
+            pictureBox1.Load(p.PictureBox);
+            }
+        }
+        private void clear()
+        {
+            NametextBox.Text = "";
+            CreatortextBox.Text = "";
+            RatingUpDown.Value = 0;
+            RevenuetextBox.Text = "";
+            CharactertextBox.Text = "";
+            pictureBox1.Image = null;
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            pictureBox1.Load(openFileDialog1.FileName);
+        }
+
+        private void Savebutton_Click_1(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void Newbutton_Click_1(object sender, EventArgs e)
+        {
+            current = Count;
+            Count++;
+            clear();
+        }
+
+        private void Previousbutton_Click_1(object sender, EventArgs e)
+        {
+            if (current > 0)
+            {
+                Save();
+                current--;
+                Currentlabel.Text = current.ToString();
+            }
+            ShowMovie(movies[current]);
+        }
+
+        private void Lastbutton_Click_1(object sender, EventArgs e)
+        {
+            Save();
+            current = Count - 1;
+            Currentlabel.Text = current.ToString();
+            ShowMovie(movies[current]);
+        }
+
+        private void Firstbutton_Click_1(object sender, EventArgs e)
+        {
+            Save();
+            current = 0;
+            Currentlabel.Text = current.ToString();
+            ShowMovie(movies[current]);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Nextbutton_Click_1(object sender, EventArgs e)
+        {
+            if (current < Count - 1)
+            {
+                Save();
+                current++;
+                Currentlabel.Text = current.ToString();
+            }
+            ShowMovie(movies[current]);
+        }
+    }
+}
